7f473ed95eb518ce94b38d1602c2c2b1c198e81d
yhk <yhk@seoulauction.com>
2022-03-29 17:30:02 +0900

    부트 세팅 업로드.

---
+++ src/main/java/com/seoulauction/renewal/SeoulauctionRenewalApplication.java
@@ -0,0 +1,13 @@
+package com.seoulauction.renewal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeoulauctionRenewalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeoulauctionRenewalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/seoulauction/renewal/SeoulauctionRenewalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seoulauction.renewal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeoulauctionRenewalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
