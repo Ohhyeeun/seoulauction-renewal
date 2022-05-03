ebc5051898d06c20c15a929b04077c570d3b1998
yhk <yhk@seoulauction.com>
2022-05-03 15:49:58 +0900

    main 작업중.

---
+++ src/main/java/com/seoulauction/renewal/service/MainService.java
@@ -0,0 +1,19 @@
+package com.seoulauction.renewal.service;
+
+import com.seoulauction.renewal.domain.CommonMap;
+import com.seoulauction.renewal.mapper.aws.AWSTestMapper;
+import com.seoulauction.renewal.mapper.kt.KTTestMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class MainService {
+
+    private final KTTestMapper ktTestMapper;
+
+    public void
+
+}
