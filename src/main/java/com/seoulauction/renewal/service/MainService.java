d7a79ebfe222b14f753aefb4a5a911e13a26fe0a
yhk <yhk@seoulauction.com>
2022-05-06 11:09:01 +0900

    충돌 해결 부분 빠진거 푸쉬.

--- src/main/java/com/seoulauction/renewal/service/MainService.java
+++ src/main/java/com/seoulauction/renewal/service/MainService.java
@@ -1,13 +1,8 @@
 package com.seoulauction.renewal.service;
 
 import com.seoulauction.renewal.domain.CommonMap;
-<<<<<<< HEAD
-import com.seoulauction.renewal.mapper.aws.AWSTestMapper;
-import com.seoulauction.renewal.mapper.kt.KTTestMapper;
-=======
 import com.seoulauction.renewal.exception.SAException;
 import com.seoulauction.renewal.mapper.aws.MainMapper;
->>>>>>> develop
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
