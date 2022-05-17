b2f1bb2a9a497ba16b8953941fce8657a66b2f02
송가현\rkgus <rkgus8409@gmail.com>
2022-05-17 14:08:48 +0900

    비지니스 로직 위치 수정(controller -> service)

--- src/main/java/com/seoulauction/renewal/service/LoginService.java
+++ src/main/java/com/seoulauction/renewal/service/LoginService.java
@@ -1,8 +1,12 @@
 package com.seoulauction.renewal.service;
 
+import org.apache.commons.collections.MapUtils;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.seoulauction.renewal.common.RestResponse;
 import com.seoulauction.renewal.domain.CommonMap;
+import com.seoulauction.renewal.exception.SAException;
 import com.seoulauction.renewal.mapper.kt.LoginMapper;
 
 import lombok.RequiredArgsConstructor;
@@ -32,9 +36,12 @@ public class LoginService {
     
 
     public CommonMap selectCustLoginIdByCustName(CommonMap paramMap){
-        return loginMapper.selectCustLoginIdByCustName(paramMap);
+    	CommonMap resultMap = loginMapper.selectCustLoginIdByCustName(paramMap);
+    	if(!MapUtils.isEmpty(resultMap)) {
+			 throw new SAException("일치하는 회원 정보가 없습니다.");
+		 }
+        return resultMap;
     }
-  
 
     public int updatePasswordByLoginId(CommonMap commonMap){  
     	return loginMapper.updatePasswordByLoginId(commonMap);
