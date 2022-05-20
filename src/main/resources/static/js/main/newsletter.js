9d2f491050d805582cc14a3c7c9f53bfa54997f2
yhk <yhk@seoulauction.com>
2022-05-20 15:27:03 +0900

    뉴스레터 구독기능 안되던 부분 수정.

--- src/main/resources/static/js/main/newsletter.js
+++ src/main/resources/static/js/main/newsletter.js
@@ -10,26 +10,9 @@ $(document).ready(function(){
 
     $('.subscriptBtn').click(function(){
 
+        console.log('asdfasdfasdf');
+
         if(subscript) {
-            $('.newsletter-blackBg').fadeIn('fast');
-            $('#terms').show();
-            $('#termsResult').hide();
-            $('.newsAgree-close').click(function () {  /*닫기 버튼 */
-                $('.newsletter-blackBg').fadeOut('fast');
-            });
-        } else {
-            alert("개인 정보 수집에 동의해주세요.");
-        }
-    });
-
-    /* 뉴스레터 개인정보 동의 팝업 */
-    $('#newsAgree').click(function(){
-        newsAgree = $('#newsAgree').prop('checked');
-    });
-
-    $('.newsAgree-btn').click(function(){
-
-        if(newsAgree){
 
             let email = $('#newsEmail').val();
             let name = $('#newsName').val();
@@ -39,7 +22,7 @@ $(document).ready(function(){
             data['name'] = name;
             data['lang'] = document.documentElement.lang;
             if(!email || !name){
-                alert('이메일 혹은 이름이 올바르지않습니다.');
+                alert('이메일 혹은 이름이 올바르지 않습니다.');
                 return;
             }
 
@@ -51,19 +34,21 @@ $(document).ready(function(){
                     if(!success){
                         alert(result.data.msg);
                     } else {
-                        $('#terms').hide();
-                        $('#termsResult').fadeIn(400);
+                        $('.newsletter-comfirmbox').fadeIn();
+                        $('.newsletter-blackBg').fadeIn('fast');
+                        $('#newsEmail').val('');
+                        $('#newsName').val('');
                     }
                 })
                 .catch(function(error){
                     console.log(error);
                 });
 
+
         } else {
             alert("개인 정보 수집에 동의해주세요.");
         }
     });
-
     $('.newsAgree-comfirmbtn').click(function(){
         $('.newsletter-blackBg').fadeOut('fast');
     });
