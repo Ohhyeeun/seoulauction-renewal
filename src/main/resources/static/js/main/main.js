769f2332a816ac5121652a74d94357a0f50a1da8
csy-seoulauction <csy@seoulauction.com>
2022-05-25 14:14:29 +0900

    이미지 S3file API 호출 적용

--- src/main/resources/static/js/main/main.js
+++ src/main/resources/static/js/main/main.js
@@ -331,12 +331,14 @@ function loadBeltBanner() {
             const success =  response.data.success;
             if (success) {
                 const bannerList = response.data.data;
+                console.log(bannerList); //tag: pc_ko, pc_en, mo_ko, mo_en
                 bannerList.map(item => {
                     const content = JSON.parse(item.content);
+                    console.log(item.image)
                     const returnDom =  `<div class="swiper-slide platform-bg" style="background-color: ${content.backgroundColor} ">
                                             <a href="${ locale === 'en' ? content.url_en : content.url_ko }" target="_blank"  >
-                                                <img src="${locale === 'en' ? content.image_pc_en_url : content.image_pc_ko_url }" alt="beltPcBanner" class="beltBannerImg-pc platform-img" >
-                                                <img src="${locale === 'en' ? content.image_mo_en_url : content.image_mo_ko_url }" alt="beltMobileBanner" class="beltBannerImg-mo platform-img" >
+                                                <img src="${locale === 'en' ? item.image.pc_en_url : item.image.pc_ko_url }" alt="beltPcBanner" class="beltBannerImg-pc platform-img" >
+                                                <img src="${locale === 'en' ? item.image.mo_en_url : item.image.mo_ko_url }" alt="beltMobileBanner" class="beltBannerImg-mo platform-img" >
                                             </a>
                                         </div>`;
 
