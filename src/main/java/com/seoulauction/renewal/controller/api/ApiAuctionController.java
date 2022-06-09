a48390922b2324fd55f5f03fc28f82b4e56fd1ce
sjk-seoulauction <sjk@seoulauction.com>
2022-06-09 16:58:04 +0900

    패들번호 임시 커밋

--- src/main/java/com/seoulauction/renewal/controller/api/ApiAuctionController.java
+++ src/main/java/com/seoulauction/renewal/controller/api/ApiAuctionController.java
@@ -1,13 +1,20 @@
 package com.seoulauction.renewal.controller.api;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.seoulauction.renewal.common.RestResponse;
 import com.seoulauction.renewal.common.SAConst;
 import com.seoulauction.renewal.domain.CommonMap;
+import com.seoulauction.renewal.domain.SAUserDetails;
 import com.seoulauction.renewal.service.AuctionService;
+import com.seoulauction.renewal.util.SecurityUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @Log4j2
@@ -19,5 +26,32 @@ public class ApiAuctionController {
     @PostMapping("/paddle")
     public ResponseEntity<RestResponse> paddle(@RequestBody CommonMap paramMap) {
         return ResponseEntity.ok(RestResponse.ok(auctionService.insertPaddle(paramMap)));
+    }
+
+    @GetMapping("/paddles/{saleNo}")
+    public ResponseEntity<RestResponse> getPaddle(@PathVariable("saleNo") int saleNo) {
+        CommonMap paramMap = new CommonMap();
+        paramMap.put("sale_no", saleNo);
+        return ResponseEntity.ok(RestResponse.ok(auctionService.selectSalePaddNo(paramMap)));
+    }
+
+    @GetMapping(value = "/progress")
+    public ResponseEntity<RestResponse> progress(){
+        CommonMap map = new CommonMap();
+        return ResponseEntity.ok(RestResponse.ok(auctionService.selectProgressSaleList(map)));
+    }
+
+    @GetMapping(value = "/scheduled")
+    public ResponseEntity<RestResponse> scheduled(){
+        CommonMap map = new CommonMap();
+        return ResponseEntity.ok(RestResponse.ok(auctionService.selectScheduledSaleList(map)));
+    }
+
+    @GetMapping(value="/sales/{saleNo}")
+    public ResponseEntity<RestResponse> sale(@PathVariable("saleNo") int saleNo) {
+        CommonMap commonMap = new CommonMap();
+        commonMap.put("sale_no", saleNo);
+
+        return ResponseEntity.ok(RestResponse.ok(auctionService.selectSaleInfo(commonMap)));
     }
 }
