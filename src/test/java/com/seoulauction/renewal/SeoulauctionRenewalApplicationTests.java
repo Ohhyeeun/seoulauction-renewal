b140c489895af8d736e2becc6ab291c717984ae0
yhk <yhk@seoulauction.com>
2022-06-28 18:22:42 +0900

    Mapper 소문자 -> 대문자로 변경 작업.

--- src/test/java/com/seoulauction/renewal/SeoulauctionRenewalApplicationTests.java
+++ src/test/java/com/seoulauction/renewal/SeoulauctionRenewalApplicationTests.java
@@ -1,5 +1,6 @@
 package com.seoulauction.renewal;
 
+import com.seoulauction.renewal.auth.PasswordEncoderAESforSA;
 import com.seoulauction.renewal.service.MainService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,9 +12,12 @@ class SeoulauctionRenewalApplicationTests {
     @Autowired
     MainService mainService;
 
+    @Autowired
+    PasswordEncoderAESforSA ddd;
+
     @Test
     public void test(){
-        mainService.selectAuctions(null);
+        System.out.println(ddd.encode("12345678"));
     }
 
 }
