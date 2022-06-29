056d576904404758f80a378728fe0df8debe440a
yhk <yhk@seoulauction.com>
2022-06-29 20:18:06 +0900

    연혁 정리

--- src/main/java/com/seoulauction/renewal/config/MvcConfig.java
+++ src/main/java/com/seoulauction/renewal/config/MvcConfig.java
@@ -27,8 +27,6 @@ public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
 
-        registry.addResourceHandler("/favicon.ico").addResourceLocations("classpath:/static/").setCachePeriod(CASH_PERIOD);
-
         registry.addResourceHandler("/js/**").addResourceLocations("classpath:/static/js/").setCachePeriod(CASH_PERIOD);
         registry.addResourceHandler("/css/**").addResourceLocations("classpath:/static/css/").setCachePeriod(CASH_PERIOD);
         registry.addResourceHandler("/images/**").addResourceLocations("classpath:/static/images/").setCachePeriod(CASH_PERIOD);
