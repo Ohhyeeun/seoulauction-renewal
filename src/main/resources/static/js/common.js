762bb779a406fec28ebb91b3414748020f582572
kimyooong <yhk@seoulauction.com>
2022-07-13 19:09:48 +0900

    모바일 네비바 수정.

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -1211,15 +1211,8 @@ function nativeToggleMenu(state) {
     const menugElement = document.querySelector('.submenuBg');
 
     if (bgElement.classList.contains('on') && menugElement.classList.contains('on')) {
-  //    window.alert(document.querySelector('.submenuBg-closeBtn').innerHTML);
-        window.alert(bgElement.classList);
-        window.alert(menugElement.classList);
       document.querySelector('.submenuBg-closeBtn').click();
     } else {
-   //   window.alert(document.querySelector('.m-gnbmenu').innerHTML)
-        window.alert(bgElement.classList);
-        window.alert(menugElement.classList);
-        document.querySelector('.m-gnbmenu').click();
-
+      document.querySelector('.m-gnbmenu').click();
     }
 }
