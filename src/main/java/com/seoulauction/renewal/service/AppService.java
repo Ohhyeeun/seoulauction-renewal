bb4f9502f73244e756c09c295def8bd817ef86a1
SunwooJo <sunwoo.jo88@gmail.com>
2022-07-15 13:48:06 +0900

    로그인 유지 API - DB 테이블 설계 및 생성, Query문 작성 완료, 앱 초기 Setting 마무리 중 앱 정보 저장 API - DB 테이블 설계 및 생성, Query문 작성 완료, 앱 초기 Setting 중

--- src/main/java/com/seoulauction/renewal/service/AppService.java
+++ src/main/java/com/seoulauction/renewal/service/AppService.java
@@ -1,12 +1,10 @@
 package com.seoulauction.renewal.service;
 
-import com.seoulauction.renewal.common.RestResponse;
 import com.seoulauction.renewal.domain.CommonMap;
 import com.seoulauction.renewal.exception.SAException;
 import com.seoulauction.renewal.mapper.kt.AppMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -33,24 +31,27 @@ public class AppService {
         List<CommonMap> resultList = appMapper.selectLoginToken(paramMap); //로그인 정보로 토큰 조회
         CommonMap map = new CommonMap();
         if(resultList.size() < 1) {
-            //JWT Token을 생성하는 로직 구현
+            System.out.println("Token 조회 결과 없음 => Token 생성 및 insert");
+            //JWT Token을 생성하는 로직 구현 요망
             String tokenVal = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9.dyt0CoTl4WoVjAHI9Q_CwSKhl6d_9rhM3NrXuJttkao";
             paramMap.put("token", tokenVal); //토큰 값
             //토큰 만료기간은 토큰 생성 시간으로부터 1년
             appMapper.insertLoginToken(paramMap);
 
             resultList = appMapper.selectLoginToken(paramMap);
+//            System.out.println("resultList = "+resultList);
 
-            map.put("id", resultList.get(0).get("cust_no")); //고객번호
+            map.put("id", resultList.get(0).get("CUST_NO")); //고객번호
             map.put("token", tokenVal); //토큰 값
-            map.put("expiredAt", resultList.get(0).get("expired_dt")); //토큰 만료기간
+            map.put("expiredAt", resultList.get(0).get("EXPIRED_DT")); //토큰 만료기간
         }
         else {
+            System.out.println("Token 조회 결과 있음 => 기존 토큰에 로그인시간, 토큰 만료기간 update");
             appMapper.updateLoginToken(paramMap); //로그인시간, 토큰 만료기간 update
 
-            map.put("id", resultList.get(0).get("cust_no")); //고객번호
-            map.put("token", resultList.get(0).get("token")); //토큰 값
-            map.put("expiredAt", resultList.get(0).get("expired_dt")); //토큰 만료기간
+            map.put("id", resultList.get(0).get("CUST_NO")); //고객번호
+            map.put("token", resultList.get(0).get("TOKEN")); //토큰 값
+            map.put("expiredAt", resultList.get(0).get("EXPIRED_DT")); //토큰 만료기간
         }
 
         return map;
@@ -64,14 +65,19 @@ public class AppService {
         return result;
     }
 
-    public List<CommonMap> selectLoginByToken(CommonMap paramMap) {
-        List<CommonMap> resultList = appMapper.selectLoginToken(paramMap); //토큰 값으로 조회
+    public List<CommonMap> selectLoginByToken(String token) {
+//        if(paramMap.get("token") == null && paramMap.get("token") == "") throw new SAException("앱의 토큰 정보가 필요합니다.");
+        if(token == null && token.equals("")) throw new SAException("앱의 토큰 정보가 필요합니다.");
+
+        CommonMap map = new CommonMap();
+        map.put("token", token); //토큰 값
+        List<CommonMap> resultList = appMapper.selectLoginToken(map); //토큰 값으로 조회
         if(resultList.size() > 0) {
-            paramMap.put("cust_no", resultList.get(0).get("cust_no")); //토큰 값
-            appMapper.updateLoginToken(paramMap); //로그인시간, 토큰 만료기간 update
+            map.put("cust_no", resultList.get(0).get("CUST_NO")); //고객번호
+            appMapper.updateLoginToken(map); //로그인시간, 토큰 만료기간 update
             return resultList;
         } else {
-            throw new SAException("토큰 정보를 찾을 수 없습니다");
+            throw new SAException("요청하신 토큰 정보를 찾을 수 없습니다");
         }
     }
 }
