26a3579e8a5b472d2f7a4b234ef49f5d1837dd07
KennyAuction <khp@seoulauction.com>
2022-07-27 16:25:46 +0900

    온라인경매 수정완료

--- src/main/resources/static/js/simple.js
+++ src/main/resources/static/js/simple.js
@@ -1,49 +1,56 @@
-var subscriber = new llnwrtssdk.Subscriber();
-var subscriber2 = new llnwrtssdk.Subscriber();
-// subscribe to events
-subscriber.on('*',handleSubscriberEvent);
-(function(llnwrtssdk) {
-  'use strict';
+
+let subscriber = new llnwrtssdk.Subscriber();
+let subscriber2 = new llnwrtssdk.Subscriber();
+
+if (matchMedia("all and (min-width: 1024px)").matches) {
+    subscriber2.off('*',handleSubscriberEvent);
+    // subscribe to events
+    subscriber.on('*',handleSubscriberEvent);
+    (function(llnwrtssdk) {
+        'use strict';
         console.log('Starting!');
         var subscribe_config = {
-                mediaElement: document.getElementById('llnw-rts-subscriber'),
-                host: 'selauction-rts.s.llnwi.net',
-                shortName: 'selauction',
-                streamName: 'mg',
+            mediaElement: document.getElementById('llnw-rts-subscriber'),
+            host: 'selauction-rts.s.llnwi.net',
+            shortName: 'selauction',
+            streamName: 'mg',
         };
         subscriber.init(subscribe_config)
-                        .then(function () {
-                                console.log('Subscribing!');
+            .then(function () {
+                console.log('Subscribing!');
                 return subscriber.subscribe().then(function () {
-                                console.log('Playing!');
-                        })
-                        .catch(function (err) {
-                                console.log('Could not play: ' + err);
-                        });
-        });
-})(window.llnwrtssdk);
+                    console.log('Playing!');
+                })
+                    .catch(function (err) {
+                        console.log('Could not play: ' + err);
+                    });
+            });
+    })(window.llnwrtssdk);
+} else {
+    subscriber.off('*',handleSubscriberEvent);
+    subscriber2.on('*',handleSubscriberEvent);
+    (function(llnwrtssdk) {
+        'use strict';
+        console.log('Starting!');
+        var subscribe_config2 = {
+            mediaElement: document.getElementById('llnw-rts-subscriber2'),
+            host: 'selauction-rts.s.llnwi.net',
+            shortName: 'selauction',
+            streamName: 'mg',
+        };
+        subscriber2.init(subscribe_config2)
+            .then(function () {
+                console.log('Subscribing!');
+                return subscriber2.subscribe().then(function () {
+                    console.log('Playing!');
+                })
+                    .catch(function (err) {
+                        console.log('Could not play: ' + err);
+                    });
+            });
+    })(window.llnwrtssdk);
+}
 
-subscriber2.on('*',handleSubscriberEvent);
-(function(llnwrtssdk) {
-    'use strict';
-    console.log('Starting!');
-    var subscribe_config2 = {
-        mediaElement: document.getElementById('llnw-rts-subscriber2'),
-        host: 'selauction-rts.s.llnwi.net',
-        shortName: 'selauction',
-        streamName: 'mg',
-    };
-    subscriber2.init(subscribe_config2)
-        .then(function () {
-            console.log('Subscribing!');
-            return subscriber2.subscribe().then(function () {
-                console.log('Playing!');
-            })
-                .catch(function (err) {
-                    console.log('Could not play: ' + err);
-                });
-        });
-})(window.llnwrtssdk);
 //---------------------------------------------------------------------------
 function handleSubscriberEvent (event) {
   // The name of the event:
@@ -54,3 +61,54 @@ function handleSubscriberEvent (event) {
   console.log(type);
   //based on the type of event .. you may want to look further into subsriver/data objects
 }
+
+$(window).resize(function(){
+    if (matchMedia("all and (min-width: 1024px)").matches) {
+        subscriber2.off('*',handleSubscriberEvent);
+        // subscribe to events
+        subscriber.on('*',handleSubscriberEvent);
+        (function(llnwrtssdk) {
+            'use strict';
+            console.log('Starting!');
+            var subscribe_config = {
+                mediaElement: document.getElementById('llnw-rts-subscriber'),
+                host: 'selauction-rts.s.llnwi.net',
+                shortName: 'selauction',
+                streamName: 'mg',
+            };
+            subscriber.init(subscribe_config)
+                .then(function () {
+                    console.log('Subscribing!');
+                    return subscriber.subscribe().then(function () {
+                        console.log('Playing!');
+                    })
+                        .catch(function (err) {
+                            console.log('Could not play: ' + err);
+                        });
+                });
+        })(window.llnwrtssdk);
+    } else {
+        subscriber.off('*',handleSubscriberEvent);
+        subscriber2.on('*',handleSubscriberEvent);
+        (function(llnwrtssdk) {
+            'use strict';
+            console.log('Starting!');
+            var subscribe_config2 = {
+                mediaElement: document.getElementById('llnw-rts-subscriber2'),
+                host: 'selauction-rts.s.llnwi.net',
+                shortName: 'selauction',
+                streamName: 'mg',
+            };
+            subscriber2.init(subscribe_config2)
+                .then(function () {
+                    console.log('Subscribing!');
+                    return subscriber2.subscribe().then(function () {
+                        console.log('Playing!');
+                    })
+                        .catch(function (err) {
+                            console.log('Could not play: ' + err);
+                        });
+                });
+        })(window.llnwrtssdk);
+    }
+});
