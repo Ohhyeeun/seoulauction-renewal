893efffd5b85e9e973c713ce6b41d152b866a775
henry hong <yih@seoulauction.com>
2022-08-04 14:50:00 +0900

    안드로이드 Path 변경

--- src/main/resources/static/js/native.js
+++ src/main/resources/static/js/native.js
@@ -88,7 +88,8 @@ $(function() {
     e.preventDefault();
     const deviceInfo = await getDeviceInfo();
     if (deviceInfo?.os === 'android') {
-      openWebBrowser('https://play.google.com/store/apps/details?id=seoulauction.seoulauction&hl=ko&gl=US');
+      // openWebBrowser('https://play.google.com/store/apps/details?id=seoulauction.seoulauction&hl=ko&gl=US');
+      openWebBrowser('market://launch?id=seoulauction.seoulauction');
     }
 
     if (deviceInfo?.os === 'ios') {
